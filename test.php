f3c31368ac9c85fe8fa177123c75b884d4c8884f
MRxwr <nasserhatab@gmail.com>
2023-11-25 03:00:02 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,18 @@
 <?php
-
-$html = file_get_contents("https://egydead.space/?s=%D9%83%D9%88%D9%86%D8%A7%D9%86");
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://egydead.space/?s=%D9%83%D9%88%D9%86%D8%A7%D9%86',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+$html = $response;
 
 // Create a DOMDocument with specified character encoding
 $dom = new DOMDocument('1.0', 'UTF-8');
