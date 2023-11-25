713db52bb03c2dee4deb04ae04e8cce9c5c2c544
MRxwr <nasserhatab@gmail.com>
2023-11-25 03:16:55 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,50 +1,48 @@
 <?php
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://www.youtube.com/',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    ));
-    var_dump($response = curl_exec($curl));
-    curl_close($curl);
+
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://egydead.space/wp-content/themes/egydeadc-taq/Ajax/live-search.php',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('search' => 'Detective Conan'),
+  CURLOPT_HTTPHEADER => array(
+    'X-Requested-With: XMLHttpRequest'
+  ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
 $html = $response;
 
-// Create a DOMDocument with specified character encoding
-$dom = new DOMDocument('1.0', 'UTF-8');
-@$dom->loadHTML('<?xml encoding="UTF-8">' . $html);
+// Create a DOMDocument
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
 
 // Create a DOMXPath object
 $xpath = new DOMXPath($dom);
 
 // Query the HTML to extract data
-$movieItems = $xpath->query('//li[@class="movieItem"]');
+$liveItems = $xpath->query('//div[@class="liveItem"]');
 $data = [];
 
-// Loop through each movie item
-foreach ($movieItems as $movieItem) {
-    $movie = [
-        'url' => $xpath->evaluate('string(a/@href)', $movieItem),
-        'title' => $xpath->evaluate('string(a/@title)', $movieItem),
-        'image' => $xpath->evaluate('string(a/img/@src)', $movieItem),
-        'bottomTitle' => $xpath->evaluate('string(a/h1[@class="BottomTitle"])', $movieItem),
-        'category' => $xpath->evaluate('string(a/span[@class="cat_name"])', $movieItem),
-        'episode' => [
-            'label' => $xpath->evaluate('string(a/span[@class="number_episode"]/b)', $movieItem),
-            'number' => $xpath->evaluate('string(a/span[@class="number_episode"]/em)', $movieItem),
-        ],
+// Loop through each liveItem
+foreach ($liveItems as $liveItem) {
+    $jsonData = [
+        'liveItem' => [
+            'href' => $xpath->evaluate('string(a/@href)', $liveItem),
+            'imgSrc' => $xpath->evaluate('string(a/img/@src)', $liveItem),
+            'title' => $xpath->evaluate('string(a/h3)', $liveItem),
+        ]
     ];
 
-    // Add the movie data to the array
-    $data[] = $movie;
+    // Add the JSON data to the array
+    $data[] = $jsonData;
 }
 
-// Convert the array to JSON
-$jsonData = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
-// Output the JSON
-echo $jsonData;
+// Output the JSON array
+echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
