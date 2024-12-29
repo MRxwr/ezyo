66add7657ed19f8a363dcc255e708f31f52c8d42
MRxwr <nasserhatab@gmail.com>
2024-12-29 11:54:35 +0300

    Update search.php

--- request/search.php
+++ request/search.php
@@ -2,6 +2,7 @@
 //get tags
 if ( $tags = selectDB('tags',"`status` LIKE '0'") ){
 	for( $i = 0 ; $i < sizeof($tags) ; $i++ ){
+		$tags[$i]["id"] = (STRING)$tags[$i]["id"];
 		unset($tags[$i]["userId"],$tags[$i]["status"],$tags[$i]["date"]);
 	}
 	$response["tags"] = $tags;
@@ -13,6 +14,7 @@ if ( $tags = selectDB('tags',"`status` LIKE '0'") ){
 //get banners
 if( $banners = selectDB('banners',"`status` LIKE '0'") ){
 	for( $i = 0 ; $i < sizeof($banners) ; $i++ ){
+		$banners[$i]["id"] = (STRING)$banners[$i]["id"];
 		for( $y = 0 ; $y < sizeof($unsetData) ; $y++ ){
 			unset($banners[$i][$unsetData[$y]]);
 		}
@@ -26,6 +28,9 @@ if( $banners = selectDB('banners',"`status` LIKE '0'") ){
 //get Vendors
 if ( $vendors = selectDB('vendors',"`status` LIKE '0' AND (`enShop` LIKE '%{$_GET["key"]}%' OR `arShop` LIKE '%{$_GET["key"]}%')")){
 	for( $i = 0 ; $i < sizeof($vendors) ; $i++ ){
+		$vendors[$i]["id"] = (STRING)$vendors[$i]["id"];
+		$vendors[$i]["is_new"] = (STRING)$vendors[$i]["is_new"];
+		$vendors[$i]["is_busy"] = (STRING)$vendors[$i]["is_busy"];
 		for( $y = 0 ; $y < sizeof($unsetData) ; $y++ ){
 			unset($vendors[$i][$unsetData[$y]]);
 		}
