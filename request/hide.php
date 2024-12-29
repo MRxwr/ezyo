2e2a0ddcd3dbc2b72eb372a87b833feea22604a6
MRxwr <nasserhatab@gmail.com>
2024-12-29 10:31:30 +0300

    Update hide.php

--- request/hide.php
+++ request/hide.php
@@ -1,6 +1,6 @@
 <?php
 if( $settings = selectDB('settings',"`id` = '1'") ){
-	$response["hide"] = $settings[0]["hideSocial"];
+	$response["hide"] = (string)$settings[0]["hideSocial"];
 }
 echo outputData($response);die();
 ?>
